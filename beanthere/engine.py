37381f2ba801d8793d953da3e9447e2a77534505
Elvin <elvin.mwarangu@student.moringaschool.com>
2025-12-04 16:40:40 +0300

    did changes

--- beanthere/engine.py
+++ beanthere/engine.py
@@ -5,8 +5,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "sqlite:///beanthere.db"
 
 engine = create_engine(DATABASE_URL, echo=False, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 Base = declarative_base()
 
+
 def get_session():
+    """Returns a session wrapped for context-manager use."""
     return SessionLocal()
